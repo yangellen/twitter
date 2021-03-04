ef04de3e17613ca80204e593f6c20937e1992023
Ellen Yang <yange@oregonstate.edu>
2021-03-04 10:48:15 -0800

    user can view tweets with the user profile picture, username, and tweet text

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Ellen Yang on 3/3/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+   @IBOutlet weak var profileImageView: UIImageView!
+   @IBOutlet weak var userNameLabel: UILabel!
+   @IBOutlet weak var tweetContent: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
